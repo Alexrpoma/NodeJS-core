3aa64c9775f15fde0d628409aba6f350219ee911
Alex Poma <alex.rpoma@outlook.com>
2023-07-24 13:20:48 -0400

    global variable

---
+++ globalVariable.js
@@ -0,0 +1,16 @@
+/*
+  GLOBAL VARIABLE:
+
+  nodeJs                      navigator
+  
+
+  global      globalThis      window
+
+  from global variable we can access to every functions of javascript.
+*/
+
+//Example:
+
+globalThis.console.log(global);
+globalThis.console.log("Hello");
+globalThis.console.log(globalThis.Math.pow(2,2));
