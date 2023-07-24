dbf1a391a57131ada5d1ea732e3c2f7cd551fb48
Alex Poma <alex.rpoma@outlook.com>
2023-07-24 16:51:52 -0400

    common modules - os

---
+++ nativeModules/os/index.js
@@ -0,0 +1,11 @@
+const os = require('node:os')
+
+console.log('Operating System')
+console.log(('---------------'))
+console.log(`name ${os.platform}`)
+console.log(`version ${os.release}`)
+console.log(`architecture ${os.arch}`)
+console.log(`CPUs ${os.cpus().length}`) // --> For scaling processes in NodeJs.
+os.cpus().forEach(c => console.log(c))
+console.log(`Free memory ${os.freemem / 1024 / 1024}mb`)
+console.log(`Total memory ${os.totalmem / 1024 / 1024}mb`)
